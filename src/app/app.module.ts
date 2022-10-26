a1b9484de734f8d35ee03d691b77ca578c536a8e
Multitudinis <lucaspessoa.ds@outlook.com>
2022-10-25 22:45:19 -0300

    spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -14,7 +13,6 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule,
     MatToolbarModule,
     HttpClientModule
   ],
--- src/app/tarefas/services/tarefas.service.ts
+++ src/app/tarefas/services/tarefas.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 
 import { Tarefa } from '../model/tarefa';
+import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,11 @@ export class TarefasService {
   constructor(private httpClient: HttpClient) { }
 
   list(){
-    return this.httpClient.get<Tarefa[]>(this.API);
+    return this.httpClient.get<Tarefa[]>(this.API)
+    .pipe(
+      first(),
+      delay(5000),
+      tap(tarefas => console.log(tarefas))
+    )
   }
 }
